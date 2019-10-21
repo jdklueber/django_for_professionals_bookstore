6ee7a77ee81037c1bdd51a6f3d546065bc4e0872
jklueber@thesoftwareguild.com <jklueber@thesoftwareguild.com>
2019-10-20 20:20:15 -0400

    Added about page, static file handling, and bootstrapification

--- pages/tests.py
+++ pages/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase, SimpleTestCase
 from django.urls import reverse, resolve
-from .views import HomePageView
+from .views import HomePageView, AboutPageView
 
 class HomePageTests(SimpleTestCase):
     def setUp(self):
@@ -16,9 +16,24 @@ class HomePageTests(SimpleTestCase):
     def test_home_page_contains_correct_html(self):
         self.assertContains(self.response, "Homepage")
     
-    def test_home_page_does_not_contain_incorrect_html(self):
-        self.assertNotContains(self.response, 'Hi there! I should not be on the page.')
-
     def test_home_page_resolves_correctly(self):
         view = resolve('/')
         self.assertEquals(view.func.__name__, HomePageView.as_view().__name__)
+
+class AboutPageTests(SimpleTestCase):
+    def setUp(self):
+        self.response = self.client.get(reverse('about'))
+
+    def test_home_page_view_by_url(self):
+        response = self.client.get('/about')
+        self.assertEquals(response.status_code, 200)
+
+    def test_home_page_template_used(self):
+        self.assertTemplateUsed(self.response, "about.html")
+
+    def test_home_page_contains_correct_html(self):
+        self.assertContains(self.response, "About")
+    
+    def test_home_page_resolves_correctly(self):
+        view = resolve('/about')
+        self.assertEquals(view.func.__name__, AboutPageView.as_view().__name__)
